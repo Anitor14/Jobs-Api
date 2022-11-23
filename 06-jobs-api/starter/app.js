98932d74deae41609c68fcc871d893dc6ca14d17
Anitor <anitorabraham@gmail.com>
2022-11-23 23:21:07 +0100

    heroku implementation

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -32,8 +32,12 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
+
 // extra packages
 
+app.get("/", (req, res) => {
+  res.send("jobs api");
+});
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
